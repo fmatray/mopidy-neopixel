53776930e7acfe05b52aaa5e415c7c73e9a0d1b8
fmatray <frederic.matray@me.com>
2020-04-14 01:06:04 +0200

    modified:   __init__.py 	modified:   ext.conf 	new file:   frontend.py 	modified:   ../setup.cfg 	modified:   ../setup.py

--- mopidy_neopixel/__init__.py
+++ mopidy_neopixel/__init__.py
@@ -5,9 +5,8 @@ import pkg_resources
 
 from mopidy import config, ext
 
-__version__ = pkg_resources.get_distribution("Mopidy-NeoPixel").version
-
-# TODO: If you need to log, use loggers named after the current Python module
+#__version__ = pkg_resources.get_distribution("Mopidy-NeoPixel").version
+__version__ = '0.1.0'
 logger = logging.getLogger(__name__)
 
 
@@ -22,28 +21,11 @@ class Extension(ext.Extension):
 
     def get_config_schema(self):
         schema = super().get_config_schema()
-        # TODO: Comment in and edit, or remove entirely
-        #schema["username"] = config.String()
-        #schema["password"] = config.Secret()
+        schema["pin"] = config.Integer()
+        schema["nb_leds"] = config.Integer()
         return schema
 
     def setup(self, registry):
-        # You will typically only implement one of the following things
-        # in a single extension.
+        from .frontend import NeoPixelFrontend
+        registry.add("frontend", NeoPixelFrontend)
 
-        # TODO: Edit or remove entirely
-        from .frontend import FoobarFrontend
-        registry.add("frontend", FoobarFrontend)
-
-        # TODO: Edit or remove entirely
-        from .backend import FoobarBackend
-        registry.add("backend", FoobarBackend)
-
-        # TODO: Edit or remove entirely
-        registry.add(
-            "http:static",
-            {
-                "name": self.ext_name,
-                "path": str(pathlib.Path(__file__).parent / "static"),
-            },
-        )
---
+++ mopidy_neopixel/frontend.py
@@ -0,0 +1,77 @@
+from __future__ import unicode_literals
+import logging
+import colorsys
+import threading
+from time import sleep
+
+from mopidy import core
+from mopidy.audio import PlaybackState
+import pykka
+import board
+import neopixel
+
+logger = logging.getLogger(__name__)
+
+class NeoPixelThread(threading.Thread):
+    def __init__(self, core, pin, nb_leds):
+        super().__init__()
+        self.name = "NeoPixel Thread"
+        self.core = core
+        self.pin, self.nb_leds = pin, nb_leds
+
+        self._stop = threading.Event()
+
+        if self.nb_leds == 0:
+            raise exceptions.FrontendError(f"NeoPixel startup failed: nb_leds must be at least 1")
+
+        pins = {
+                10: board.D10,
+                12: board.D12,
+                18: board.D18,
+                21: board.D21,
+        }
+        try: 
+            self.pixels = neopixel.NeoPixel(pins[self.pin], self.nb_leds)
+            self.pixels.fill((255, 0, 0))
+            sleep(1)
+            self.pixels.fill((0, 255, 0))
+            sleep(1)
+            self.pixels.fill((0, 0, 255))
+            sleep(1)
+        except KeyError as exc:
+            raise exceptions.FrontendError(f"NeoPixel startup failed: {exc}")
+
+    def stop(self):
+        self.pixels.fill((0, 0, 0))
+        self._stop.set()
+
+    def run(self):
+        led = 0
+        while not self._stop.isSet():
+            current_track = self.core.playback.get_current_track().get()
+            if current_track and self.core.playback.get_state().get() == PlaybackState.PLAYING:
+                length = current_track.length
+                position = self.core.playback.get_time_position().get()
+                brightness = self.core.mixer.get_volume().get()/100 if not self.core.mixer.get_mute().get() else 0   
+
+                red, green, blue = colorsys.hsv_to_rgb(position/length, 1, brightness)
+                self.pixels[led] = tuple(map(int, (red * 255, green * 255, blue * 255)))
+                led = (led + 1) % self.pixels.n 
+            else:
+                self.pixels.fill((0, 0, 0))
+            sleep(0.01)
+
+class NeoPixelFrontend(pykka.ThreadingActor, core.CoreListener):
+    def __init__(self, config, core):
+        super().__init__()
+        self.config = config['neopixel']
+        self.core = core
+
+    def on_start(self):
+        logger.info("Starting Mopidy NeoPixel")
+        self.neopixelthread = NeoPixelThread(self.core, self.config['pin'], self.config['nb_leds'] )
+        self.neopixelthread.start()
+
+    def on_stop(self):
+        logger.info("Stoping Mopidy NeoPixel")
+        self.neopixelthread.stop()
